87a5bb2198f0e1f6f5654e947bca2aee6d39a21c
unknown <lli@LLI02.corp.u4agr.com>
2015-04-15 15:22:49 +0200

    Upload the first r package.

---
+++ R/InstallPackages.R
@@ -0,0 +1,15 @@
+#' @export
+doinstall <- function(){
+  if(!require("jsonlite")){
+    install.packages("jsonlite")
+    print("jsonlite required and attempt to install")
+  }
+  library(jsonlite)
+  
+  
+  if (!require("C50")) {
+    install.packages("C50", dependencies = TRUE)
+    print("C50 required and attempt to install")
+  }
+  library(C50)
+}
